ba794e04ed0aa4512f440a13f190a1c7c3460194
Myroslav Kolodii <mkolodii@gmail.com>
2018-11-20 11:25:29 +0100

    Implement posts.

--- app/src/main/java/ua/mainacad/maintest/maintest/ui/posts/PostsListFragment.java
+++ app/src/main/java/ua/mainacad/maintest/maintest/ui/posts/PostsListFragment.java
@@ -1,7 +1,42 @@
 package ua.mainacad.maintest.maintest.ui.posts;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import ua.mainacad.maintest.maintest.R;
+import ua.mainacad.maintest.maintest.model.Post;
+import ua.mainacad.maintest.maintest.ui.users.UsersListAdapter;
+import ua.mainacad.maintest.maintest.ui.users.UsersListPresenter;
 
-public class PostsListFragment extends MvpAppCompatFragment {
+import java.util.List;
 
+public class PostsListFragment extends MvpAppCompatFragment implements IPostListView {
+    @InjectPresenter
+    PostsListPresenter mPresenter;
+    private PostsListAdapter mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.fragment_posts_list,
+                container,
+                false);
+        mAdapter = new PostsListAdapter();
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        return recyclerView;
+    }
+
+    @Override
+    public void setPostList(List<Post> posts) {
+        mAdapter.setPosts(posts);
+    }
 }
