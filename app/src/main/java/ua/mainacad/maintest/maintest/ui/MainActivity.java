3eafe3c6d59ed10b6f9c6d655465244ab8d6cb3d
formatBCE <mitnovich@gmail.com>
2018-12-19 18:48:13 +0200

    Prefs, dimens, ConstraintLayout

--- app/src/main/java/ua/mainacad/maintest/maintest/ui/MainActivity.java
+++ app/src/main/java/ua/mainacad/maintest/maintest/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package ua.mainacad.maintest.maintest.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,11 +11,13 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import ua.mainacad.maintest.maintest.R;
 import ua.mainacad.maintest.maintest.ui.login.LoginActivity;
@@ -88,13 +91,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            setFragment(new PreferenceFragment());
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -111,6 +110,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fab.setVisibility(View.GONE);
 
         if (id == R.id.nav_users) {
+            final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            if (prefs.getBoolean(
+                    getString(R.string.pref_key_auto_lock),
+                    getResources().getBoolean(R.bool.auto_lock))) {
+                Toast.makeText(
+                        this,
+                        prefs.getString(
+                                getString(R.string.pref_key_change_password_hint),
+                                getString(R.string.default_hint)),
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
             setFragment(new UsersListFragment());
         } else if (id == R.id.nav_posts) {
             setFragment(new PostsListFragment());
